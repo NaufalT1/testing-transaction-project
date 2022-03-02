7b1239b89dac82fd417757838cee12d492143fc5
Naufal <naufal.ti@outlook.com>
2022-03-02 10:50:50 +0700

    Initial commit

---
+++ src/test/java/personTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class personTest {
+    String name;
+    Integer id;
+    Integer point;
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getPoint() {
+    }
+}
